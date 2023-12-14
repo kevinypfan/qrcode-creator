c3e973c7a106016d2678a1aaa3c2cb0d46b43b9b
KevinFan <kevinypfan@gmail.com>
2023-12-14 18:25:41 +0800

    Deploy static content to GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,14 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 
 export const useRouter = () => {
-  return createBrowserRouter([{
-    path: '/',
-    element: <HomePage />
-  }]);
-}
+  return createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+    { basename: "/qrcode-creator" }
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/qrcode-creator/',
   plugins: [react()],
   server: {
     proxy: {
